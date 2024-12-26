65e42b4f4b101f618d681034ed62e81b5a91569b
Adriana Sosa <sosaadriana123@gmail.com>
2024-12-26 18:14:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const notbubu = document.getElementById('no-bubu')
 notbubu.addEventListener('click,() => {
                         window.close()
 })
+
+var audio = new Audio('https://raw.githubusercontent.com/ASSP-2005/ale-pagina/refs/heads/main/resources/tequieromuchobubu.mp3');
+    audio.play();
