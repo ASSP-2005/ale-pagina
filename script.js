61f36399487317d19da8ca2f79ea4407c9fc7834
Adriana Sosa <sosaadriana123@gmail.com>
2025-01-05 12:19:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-const notbubu = document.getElementById('no-bubu')
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('play-btn');
+const progress = document.getElementById('progress');
+// const currentTimeElement = document.getElementById('current-time');
+// const durationElement = document.getElementById('duration');
 
-notbubu.addEventListener('click,() => {
-                        window.close()
-})
+const songs = [
+  'https://audio.jukehost.co.uk/js3P2sOFgPh8XfFdUmjcSHtMTCMEIend', // Ruta de tu canción 1
+];
+let currentSongIndex = 0;
 
-// var audio = new Audio('https://raw.githubusercontent.com/ASSP-2005/ale-pagina/refs/heads/main/resources/tequieromuchobubu.mp3');
-    //audio.play();
+function updatePlayButton() {
+  if (audio.paused) {
+    playButton.textContent = '▶️';
+  } else {
+    playButton.textContent = '⏸️';
+  }
+}
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemaining = Math.floor(seconds % 60);
+  return `${minutes}:${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
+}
+
+function updateProgressBar() {
+  const progressValue = (audio.currentTime / audio.duration) * 100;
+  progress.value = progressValue;
+  currentTimeElement.textContent = formatTime(audio.currentTime);
+  durationElement.textContent = formatTime(audio.duration);
+}
+
+function setProgress() {
+  const newTime = (progress.value / 100) * audio.duration;
+  audio.currentTime = newTime;
+}
+
+function playPauseAudio() {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+  updatePlayButton();
+}
+
+
+audio.addEventListener('timeupdate', updateProgressBar);
+progress.addEventListener('input', setProgress);
+playButton.addEventListener('click', playPauseAudio);
+nextButton.addEventListener('click', nextSong);
+prevButton.addEventListener('click', prevSong);
+
+audio.addEventListener('loadedmetadata', () => {
+  durationElement.textContent = formatTime(audio.duration);
+});
+
+window.onload = () => {
+  audio.src = songs[currentSongIndex]; // Cargar la primera canción
+};
